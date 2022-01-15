ca20a5ff46d57c9d3d02d79525ea58d6e6e980ba
Yannick Forster <forstery1751@gmail.com>
2022-01-15 20:07:40 +0100

    initial project setup

---
+++ src/test/java/io/github/yaforster/badger/BadgerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yaforster.badger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
